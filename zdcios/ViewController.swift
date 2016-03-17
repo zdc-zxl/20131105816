b4659154a7ca6081c1e6372f363df46aec182e9f
zdc-zxl <dllm@123.local>
2016-03-17 11:21:30 +0800

    ios计算器

--- zdcios/ViewController.swift
+++ zdcios/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    
+    @IBAction func add(sender: UIButton) {
+        var a:Double!=0
+        var b:Double!=0
+        var c:Double!=0
+        if(!x.text!.isEmpty){
+        a = (x.text! as NSString).doubleValue
+        }
+        if(!y.text!.isEmpty){
+            b = (y.text! as NSString).doubleValue
+        }
+        c=a+b
+        z.text="\(c)"
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
