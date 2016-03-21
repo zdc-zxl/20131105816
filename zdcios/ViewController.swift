dc2711a4bb81729e17cab5832f404898fa752dd7
zdc-zxl <dllm@123.local>
2016-03-21 20:17:03 +0800

    3.21
    计算器界面

--- zdcios/ViewController.swift
+++ zdcios/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var y: UITextField!
     @IBOutlet weak var z: UITextField!
     
+    @IBOutlet weak var mark: UITextField!
+    @IBAction func mult(sender: UIButton) {
+        mark.text="*"
+    }
+ 
+   
     @IBAction func add(sender: UIButton) {
         var a:Double!=0
         var b:Double!=0
